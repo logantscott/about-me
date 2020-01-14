d8d6b77ebe1f0f778c99926773cb71aee4094f44
Logan Scott <logantscott@gmail.com>
2020-01-14 11:56:47 -0800

    update everything except the actual content (add isNo, styling, layout, update app.js)

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import isYes from './is-yes.js';
+import { isYes, isNo } from './is-yes.js';
 
 const button = document.getElementById('quizbutton');
 const quizmsg = document.getElementById('quizResults');
@@ -13,12 +13,13 @@ button.addEventListener('click', () => {
     const question2 = prompt(`${uname}, put no`);
     const question3 = prompt(`${uname}, put yes`);
     alert('See below for your results!');
-
-    //do ifYes and total the results
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    quizmsg.style.display = 'block';    //do ifYes and total the results
     let results = 0;
     let questions = 0;
     isYes(question1) ? results++ : questions++;
-    !isYes(question2) ? results++ : questions++;
+    isNo(question2) ? results++ : questions++;
     isYes(question3) ? results++ : questions++;
     //total questions asked
     questions = questions + results;
@@ -26,11 +27,11 @@ button.addEventListener('click', () => {
     //display results
     console.log(results);
 
-    if(results/questions >= .7) {
+    if (results / questions >= .7) {
         quizmsg.className = 'good';
-        quizmsg.innerText = `${uname}, you got ${results} out of ${questions} questions correct! You scored ${(results/questions).toFixed(2)*100}%. Good job!`;
+        quizmsg.innerText = `${uname}, you got ${results} out of ${questions} questions correct! You scored ${(results / questions).toFixed(2) * 100}%. Good job!`;
     } else {
         quizmsg.className = 'bad';
-        quizmsg.innerText = `${uname}, you got ${results} out of ${questions} questions correct. You scored ${(results/questions).toFixed(2)*100}%. You can do better.`;
+        quizmsg.innerText = `${uname}, you got ${results} out of ${questions} questions correct. You scored ${(results / questions).toFixed(2) * 100}%. You can do better.`;
     };
 });
--- src/is-yes.js
+++ src/is-yes.js
@@ -1,5 +1,13 @@
-export default function isYes(ans) {
-    if(ans.toLowerCase() === 'yes' || ans.toLowerCase() === 'y') return true;
-    else return false;
+export function isYes(ans) {
+    if (ans.toLowerCase() === 'yes' || ans.toLowerCase() === 'y') {
+        return true;
+    } else return false;
+    // console.log("hiiiiiiii")
+};
+
+export function isNo(ans) {
+    if (ans.toLowerCase() === 'no' || ans.toLowerCase() === 'n') {
+        return true;
+    } else return false;
     // console.log("hiiiiiiii")
 };
