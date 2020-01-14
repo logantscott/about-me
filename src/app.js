74f9ea89a7478e8d8f710376503cbec8b99d44b5
Logan Scott <logantscott@gmail.com>
2020-01-14 08:59:57 -0800

    add scripts code to src/app.js and test/example.test.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,17 @@ import isYes from './is-yes.js'
 const button = document.getElementById('quizbutton')
 
 button.addEventListener('click', () => {
-    const testq = prompt("put yes");
-
-    if(isYes(testq)) alert('yay!');
+    //prompt
+    //store result
+    //do ifYes
+    //get score
+    const question1 = prompt("put yes");
+    const question2 = prompt("put no");
+    const question3 = prompt("put yes");
+    let results = 0;
+    if(isYes(question1) === true) results++;
+    if(!isYes(question2) === true) results++;
+    if(isYes(question3) === true) results++;
+    console.log(results);
+    document.getElementById('quizResults').innerText = `You got ${results} questions correct!`;
 })
--- test/example.test.js
+++ test/example.test.js
@@ -1,19 +1,59 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-import isYes from '../src/is-yes.js/index.js';
+import isYes from '../src/is-yes.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('Yes\'s', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-
-    
 
     //Act 
     // Call the function you're testing and set the result to a const
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    //assert.equal(true, false);
+
+    //_____________________________________________________
+    //ARRANGE get the Input (guess what they might write)
+    let input = 'Yes';
+    //ARRANGE - what is the expected result
+    const expect = true;
+    //ACT - get isyes of the input
+    const result = isYes(input);
+    //ASSERT - assert the isyes with the expected
+    assert.equal(result, expect);
+    input = 'YES';
+    assert.equal(result, expect);
+    input = 'Y';
+    assert.equal(result, expect);
+
+});
+
+test('No\'s', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+
+    //Assert
+    // Make assertions about what is expected valid result
+    //assert.equal(true, false);
+
+    //_____________________________________________________
+    //ARRANGE get the Input (guess what they might write)
+    let input = 'No';
+    //ARRANGE - what is the expected result
+    const expect = false;
+    //ACT - get isyes of the input
+    const result = isYes(input);
+    //ASSERT - assert the isyes with the expected
+    assert.equal(result, expect);
+    input = 'NO';
+    assert.equal(result, expect);
+    input = 'N';
+    assert.equal(result, expect);
+
 });
