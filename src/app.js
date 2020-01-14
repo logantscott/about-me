ae03a64c9177170fdfeb0d29c3fca267ba69dffe
Logan Scott <logantscott@gmail.com>
2020-01-14 11:10:49 -0800

    update styles and refactor scripts

--- src/app.js
+++ src/app.js
@@ -1,19 +1,36 @@
-import isYes from './is-yes.js'
+import isYes from './is-yes.js';
 
-const button = document.getElementById('quizbutton')
+const button = document.getElementById('quizbutton');
+const quizmsg = document.getElementById('quizResults');
 
 button.addEventListener('click', () => {
-    //prompt
-    //store result
-    //do ifYes
-    //get score
-    const question1 = prompt("put yes");
-    const question2 = prompt("put no");
-    const question3 = prompt("put yes");
+
+    //prompt & store result
+    const uname = prompt("What is your name?") || "Human";
+    const ready = confirm(`${uname}, are you ready to take this quiz?`);
+    if(!ready) return;
+    const question1 = prompt(`${uname}, put yes`);
+    const question2 = prompt(`${uname}, put no`);
+    const question3 = prompt(`${uname}, put yes`);
+    alert('See below for your results!');
+
+    //do ifYes and total the results
     let results = 0;
-    if(isYes(question1) === true) results++;
-    if(!isYes(question2) === true) results++;
-    if(isYes(question3) === true) results++;
+    let questions = 0;
+    isYes(question1) ? results++ : questions++;
+    !isYes(question2) ? results++ : questions++;
+    isYes(question3) ? results++ : questions++;
+    //total questions asked
+    questions = questions + results;
+
+    //display results
     console.log(results);
-    document.getElementById('quizResults').innerText = `You got ${results} questions correct!`;
-})
+
+    if(results/questions >= .7) {
+        quizmsg.className = 'good';
+        quizmsg.innerText = `${uname}, you got ${results} out of ${questions} questions correct! You scored ${(results/questions).toFixed(2)*100}%. Good job!`;
+    } else {
+        quizmsg.className = 'bad';
+        quizmsg.innerText = `${uname}, you got ${results} out of ${questions} questions correct. You scored ${(results/questions).toFixed(2)*100}%. You can do better.`;
+    };
+});
