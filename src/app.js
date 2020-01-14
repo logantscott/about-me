80dc7fc5e897aef197ee364c6e1139d246c82ec8
Logan Scott <logantscott@gmail.com>
2020-01-14 13:51:57 -0800

    update content of index, adjust some css

--- src/app.js
+++ src/app.js
@@ -1,27 +1,32 @@
 import { isYes, isNo, lcase } from './is-yes.js';
 
+//get the quiz elements
 const button = document.getElementById('quizbutton');
 const quizmsg = document.getElementById('quizResults');
 
+//listen for click
 button.addEventListener('click', () => {
 
     //quiz prompts & store answers
     const uname = prompt('What is your name?') || 'Human';
     const ready = confirm(`${uname}, are you ready to take this quiz?`);
-    if(!ready) return;
+    if (!ready) return;
     const question1 = prompt(`${uname}, do you like the picture of the kitten?`);
     const question2 = prompt(`${uname}, is this content?`);
     const question3 = prompt(`${uname}, is Lorem Ipsum actual latin?`);
-    alert('See below for your results!');
-    //scroll up after quiz complete
+    alert('Tallying your results!');
+
+    //scroll up after quiz complete. don't even check, just do it.
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+
     //do ifYes and total the results
     let results = 0;
     let questions = 0;
     isYes(lcase(question1)) ? results++ : questions++;
     isNo(lcase(question2)) ? results++ : questions++;
     isNo(lcase(question3)) ? results++ : questions++;
+
     //total questions asked
     questions = questions + results;
 
