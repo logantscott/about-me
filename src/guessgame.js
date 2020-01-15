6f45affce99d0e3ffe3c50e14f76756dff1dfb89
Logan Scott <logantscott@gmail.com>
2020-01-15 11:08:58 -0800

    add number bar and update logic, refactor some code

--- src/guessgame.js
+++ src/guessgame.js
@@ -7,7 +7,11 @@ const button = document.getElementById('guessButton');
 const restart = document.getElementById('restartButton');
 const message = document.getElementById('guessMessage');
 const remaining = document.getElementById('remaining');
+const clickcontainer = document.getElementById('container');
+const allowedguesses = 4;
 let guesses = 0;
+// test code for number spans
+const spans = document.getElementsByClassName('number');
 
 // function called from win/loss, update remaining and disable form
 function disabled() {
@@ -15,25 +19,24 @@ function disabled() {
     input.value = '';
     input.disabled = true;
     button.disabled = true;
+    clickcontainer.disabled = true;
 }
-
 // update messsage function
 function newMessage(str) { 
     return message.textContent = str; 
 }
 
 // initialize DOM
-remaining.textContent = 4;
+remaining.textContent = allowedguesses;
 newMessage('I\'ve picked a number. Can you guess it?');
 
-console.log(correctNumber);
+// console.log(correctNumber);
 
 // guess guessed and button clicked, handle it!
 button.addEventListener('click', () => {
     // declare variables
     const guess = Number(input.value);
     const comparison = compareNumbers(guess, correctNumber);
-
     // initialize body class
     document.body.className = '';
 
@@ -48,16 +51,17 @@ button.addEventListener('click', () => {
     guesses++;
 
     // check for loss condition first
-    if (comparison !== 0 && 4 - guesses === 0){
+    if (comparison !== 0 && allowedguesses - guesses === 0){
         document.body.className = 'bad';
         newMessage(`My number was ${correctNumber}. You LOSE!`);
+        updateSpans(comparison, guess, correctNumber);
         return disabled();
     }
-
     // guess is valid and user hasn't lost, next check if win, else check lower or higher
     if (comparison === 0) {
         document.body.className = 'good';
         newMessage(`You guessed my number in ${guesses} ${guesses === 1 ? 'guess' : 'guesses'}, it was ${guess}! You WIN!`);
+        updateSpans(comparison, guess);
         return disabled();
     } else if (comparison === -1) {
         newMessage(`${guess} is too low. Guess HIGHER!`);
@@ -66,8 +70,9 @@ button.addEventListener('click', () => {
     }
 
     // update guesses remaining tally
-    remaining.textContent = 4 - guesses;
+    remaining.textContent = allowedguesses - guesses;
     input.select();
+    updateSpans(comparison, guess);
 });
 
 // reset game button clicked, handle it!
@@ -78,13 +83,61 @@ restart.addEventListener('click', () => {
 
     // pick a new number
     correctNumber = Math.ceil(Math.random() * (20));
-    console.log(correctNumber);
+    // console.log(correctNumber);
 
     // initialize variables and DOM
     guesses = 0;
-    remaining.textContent = 4;
+    remaining.textContent = allowedguesses;
     input.value = '';
     document.body.className = '';
     input.select();
     newMessage('I\'ve picked a number. Can you guess it?');
+
+    // initialize number select
+    for (let i = 0; i < spans.length; i++) {
+        spans[i].classList.remove('disable');
+        spans[i].removeAttribute('disabled');
+        spans[i].classList.remove('good');
+        spans[i].classList.remove('bad');
+        spans[i].classList.remove('guessed');
+    }
 });
+
+// https://stackoverflow.com/questions/19655189/javascript-click-event-listener-on-class
+for (let i = 0; i < spans.length; i++) {
+    //https://stackoverflow.com/questions/42111854/javascript-addeventlistener-on-all-created-li-elements
+    spans[i].addEventListener('click', function(e) {
+        if (input.disabled === false && e.target.classList.contains('disable') === false) {
+            input.value = e.target.textContent;
+            button.click();
+        }
+    });
+}
+
+function updateSpans(comparison, guess, loss) {
+    spans[guess - 1].classList.add('guessed');
+    if (loss) {
+        for (let i = 0; i < spans.length; i++) {
+            spans[i].classList.add('disable');
+            spans[i].disabled = true;
+        }
+        spans[loss - 1].classList.add('bad');
+    }
+    if (comparison === -1) {
+        for (let i = 0; i < guess; i++) {
+            spans[i].classList.add('disable');
+            spans[i].disabled = true;
+        }
+    } else if (comparison === 1) {
+        for (let i = guess - 1; i < spans.length; i++) {
+            spans[i].classList.add('disable');
+            spans[i].disabled = true;
+        }
+    } else if (comparison === 0) {
+        for (let i = 0; i < spans.length; i++) {
+            spans[i].classList.add('disable');
+            spans[i].disabled = true;
+        }
+        spans[guess - 1].classList.add('good');
+    }
+}
--- test/guesstest.js
+++ test/guesstest.js
@@ -4,50 +4,50 @@ import compareNumbers from '../src/compareNumbers.js';
 const test = QUnit.test;
 
 test('Test for match', function(assert) {
-    //ARRANGE Set up your parameters and expectations
+    //ARRANGE
     let input = 12;
     const expect = 0;
 
-    //ACT - Call the function you're testing and set the result to a const
+    //ACT
     const result = compareNumbers(input, 12);
 
-    //ASSERT - Make assertions about what is expected valid result
+    //ASSERT
     assert.equal(result, expect);
 });
 
 test('Test for less than', function(assert) {
-    //ARRANGE Set up your parameters and expectations
+    //ARRANGE
     let input = 1;
     const expect = -1;
 
-    //ACT - Call the function you're testing and set the result to a const
+    //ACT
     const result = compareNumbers(input, 12);
 
-    //ASSERT - Make assertions about what is expected valid result
+    //ASSERT
     assert.equal(result, expect);
 });
 
 test('Test for greater than', function(assert) {
-    //ARRANGE Set up your parameters and expectations
+    //ARRANGE
     let input = 20;
     const expect = 1;
 
-    //ACT - Call the function you're testing and set the result to a const
+    //ACT
     const result = compareNumbers(input, 12);
 
-    //ASSERT - Make assertions about what is expected valid result
+    //ASSERT
     assert.equal(result, expect);
 });
 
 test('Test for falsy', function(assert) {
-    //ARRANGE Set up your parameters and expectations
+    //ARRANGE
     let input = '12';
     const expect = false;
 
-    //ACT - Call the function you're testing and set the result to a const
+    //ACT
     let result = compareNumbers(input, 12);
 
-    //ASSERT - Make assertions about what is expected valid result
+    //ASSERT
     assert.equal(result, expect);
     input = '12';
     result = compareNumbers(input, 12);
