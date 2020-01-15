2d28b041bd9303b3ccfcc6980b868e8c1070465a
Logan Scott <logantscott@gmail.com>
2020-01-15 01:13:35 -0800

    add testing, yolo

--- test/guesstest.js
+++ test/guesstest.js
@@ -1,32 +1,70 @@
 // IMPORT MODULES under test here:
-// import example from '../src/example.js';
-//import { isYes, isNo, lcase } from '../src/is-yes.js';
+import compareNumbers from '../src/compareNumbers.js';
 
 const test = QUnit.test;
 
-test('compare numbers', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
+test('Test for match', function(assert) {
+    //ARRANGE Set up your parameters and expectations
+    let input = 12;
+    const expect = 0;
 
-    //Act 
-    // Call the function you're testing and set the result to a const
+    //ACT - Call the function you're testing and set the result to a const
+    const result = compareNumbers(input, 12);
 
-    //Assert
-    // Make assertions about what is expected valid result
-    //assert.equal(true, false);
-
-    //_____________________________________________________
-    //ARRANGE get the Input (guess what they might write)
-    let input = 'Yes';
-    //ARRANGE - what is the expected result
-    const expect = true;
-    //ACT - get isyes of the input
-    const result = isYes(lcase(input));
-    //ASSERT - assert the isyes with the expected
+    //ASSERT - Make assertions about what is expected valid result
     assert.equal(result, expect);
-    input = 'YES';
-    assert.equal(result, expect);
-    input = 'Y';
-    assert.equal(result, expect);
+});
 
+test('Test for less than', function(assert) {
+    //ARRANGE Set up your parameters and expectations
+    let input = 1;
+    const expect = -1;
+
+    //ACT - Call the function you're testing and set the result to a const
+    const result = compareNumbers(input, 12);
+
+    //ASSERT - Make assertions about what is expected valid result
+    assert.equal(result, expect);
+});
+
+test('Test for greater than', function(assert) {
+    //ARRANGE Set up your parameters and expectations
+    let input = 20;
+    const expect = 1;
+
+    //ACT - Call the function you're testing and set the result to a const
+    const result = compareNumbers(input, 12);
+
+    //ASSERT - Make assertions about what is expected valid result
+    assert.equal(result, expect);
+});
+
+test('Test for falsy', function(assert) {
+    //ARRANGE Set up your parameters and expectations
+    let input = '12';
+    const expect = false;
+
+    //ACT - Call the function you're testing and set the result to a const
+    let result = compareNumbers(input, 12);
+
+    //ASSERT - Make assertions about what is expected valid result
+    assert.equal(result, expect);
+    input = '12';
+    result = compareNumbers(input, 12);
+    assert.equal(result, expect);
+    input = 21;
+    result = compareNumbers(input, 12);
+    assert.equal(result, expect);
+    input = 0;
+    result = compareNumbers(input, 12);
+    assert.equal(result, expect);
+    input = 'some string';
+    result = compareNumbers(input, 12);
+    assert.equal(result, expect);
+    input = true;
+    result = compareNumbers(input, 12);
+    assert.equal(result, expect);
+    input = [1, 2, 3];
+    result = compareNumbers(input, 12);
+    assert.equal(result, expect);
 });
