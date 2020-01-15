9db7f3bf1de688297a1ab0dc1b43aa0471ddbd2b
Logan Scott <logantscott@gmail.com>
2020-01-15 13:28:16 -0800

    add eventlistener for hitting enter in input

--- src/guessgame.js
+++ src/guessgame.js
@@ -151,3 +151,15 @@ function updateSpans(comparison, guess, loss) {
         spans[guess - 1].classList.add('good');
     }
 }
+
+// https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener('keyup', function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+        event.preventDefault();
+      // Trigger the button element with a click
+        button.click();
+    }
+});
