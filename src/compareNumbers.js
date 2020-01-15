97281b9f0dbb6c1055f43bfe9a595e83cfd04264
Logan Scott <logantscott@gmail.com>
2020-01-15 00:34:41 -0800

    update scripts to use import function to allow for testing

--- src/compareNumbers.js
+++ src/compareNumbers.js
@@ -1,1 +1,15 @@
 //export func goes here
+
+export default function compareNumbers(guess, correctNumber) {
+    if (!Number.isInteger(guess) || !guess || guess > 20 || guess < 1) return false;
+    if (guess === correctNumber) {
+        console.log('you win!');
+        return 0;
+    } else if (guess < correctNumber) {
+        console.log('Too low. Guess higher.');
+        return -1;
+    } else if (guess > correctNumber) {
+        console.log('Too high. Guess lower.');
+        return 1;
+    }
+}
--- src/guessgame.js
+++ src/guessgame.js
@@ -1,6 +1,9 @@
+import compareNumbers from './compareNumbers.js';
+
 let correctNumber = Math.floor(Math.random() * (20)) + 1;
 const response = document.getElementById('guessresponse');
 const button = document.getElementById('guessbutton');
+//const button2 = document.getElementById('guessbutton');
 const input = document.getElementById('guess');
 const restart = document.getElementById('restart');
 const remaining = document.getElementById('remaining');
@@ -9,46 +12,84 @@ let guesses = 0;
 
 function disabled() {
     remaining.textContent = '0';
-    input.disabled = 'disabled';
-    button.disabled = 'disabled';
+    input.disabled = true;
+    button.disabled = true;
 }
 
 button.addEventListener('click', () => {
     const guess = Number(document.getElementById('guess').value);
+    const comparison = compareNumbers(guess, correctNumber);
 
-    // console.log(`Correct Number: ${correctNumber}`);
-    // console.log(`Guess: ${guess}`);
+    console.log(compareNumbers(guess, correctNumber));
 
-    if (guesses <= 4 && guess !== correctNumber) {
-        if (guesses === 4) {
-            document.body.className = 'bad';
-            response.textContent = 'You have 0 guesses remaining! YOU LOSE!';
-            return disabled();
-        } else if (guess < 1 || guess > 20) {
-            response.textContent = 'Enter a number between 1 and 20...';
-            return;
-        } else if (guess > correctNumber) {
-            response.textContent = 'Guess is too high. Lower!';
-        } else if (guess < correctNumber) {
-            response.textContent = 'Guess is too low. Higher!';
-        } else {
-            response.textContent = 'Enter a NUMBER...';
-            return;
-        }
-    } else if (guesses <= 4 && guess === correctNumber) {
-        document.body.className = 'good';
-        response.textContent = 'You guessed it! You WIN!';
-        return disabled();
-    } else { //is it even possible to get here? Hacker.
-        response.textContent = 'How did you get here??';
+    //validate the user input to be a number between 1 and 20, return if false
+    if (comparison === false) {
+        document.body.className = 'warning';
+        return response.textContent = 'Enter a number between 1 and 20...';
+    }
+
+    //valid guess, add a tally
+    guesses++;
+
+    //check for loss condition first
+    if (comparison !== 0 && 4 - guesses === 0){
         document.body.className = 'bad';
+        response.textContent = 'You have 0 guesses remaining! YOU LOSE!';
         return disabled();
     }
 
+    //so far guess is valid and user hasn't lost, next check if win, else check lower or higher
+    if (comparison === 0) {
+        document.body.className = 'good';
+        response.textContent = 'You guessed it! You WIN!';
+        return disabled();
+    } else if (comparison === -1) {
+        response.textContent = 'Guess is too low. Higher!';
+    } else if (comparison === 1) {
+        response.textContent = 'Guess is too high. Lower!';
+    }
+
     //update guesses remaining tally
-    guesses++;
     remaining.textContent = 4 - guesses;
 });
+
+// button2.addEventListener('click', () => {
+//     const guess = Number(document.getElementById('guess').value);
+
+//     // console.log(`Correct Number: ${correctNumber}`);
+//     // console.log(`Guess: ${guess}`);
+//     guesses++;
+//     if (guesses <= 4 && guess !== correctNumber) {
+//         if (guesses === 4) {
+//             document.body.className = 'bad';
+//             response.textContent = 'You have 0 guesses remaining! YOU LOSE!';
+//             return disabled();
+//         } else if (guess < 1 || guess > 20) {
+//             response.textContent = 'Enter a number between 1 and 20...';
+//             guesses--;
+//             return;
+//         } else if (guess > correctNumber) {
+//             response.textContent = 'Guess is too high. Lower!';
+//         } else if (guess < correctNumber) {
+//             response.textContent = 'Guess is too low. Higher!';
+//         } else {
+//             response.textContent = 'Enter a NUMBER...';
+//             guesses--;
+//             return;
+//         }
+//     } else if (guesses <= 4 && guess === correctNumber) {
+//         document.body.className = 'good';
+//         response.textContent = 'You guessed it! You WIN!';
+//         return disabled();
+//     } else { //is it even possible to get here? Hacker.
+//         response.textContent = 'How did you get here??';
+//         document.body.className = 'bad';
+//         return disabled();
+//     }
+
+//     //update guesses remaining tally
+//     remaining.textContent = 4 - guesses;
+// });
 
 restart.addEventListener('click', () => {
     input.removeAttribute('disabled');
